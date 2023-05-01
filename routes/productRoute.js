4bf159714c79b6737e197dea63fecac8775551ca
MayurDeore333 <122891156+MayurDeore333@users.noreply.github.com>
2023-05-01 17:40:40 +0530

    Add files via upload

---
+++ routes/productRoute.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const protect = require("../middleWare/authMiddleware");
+const {
+  createProduct,
+  getProducts,
+  getProduct,
+  deleteProduct,
+  updateProduct,
+  addSale,
+  addPurchase,
+} = require("../controllers/productController");
+const { upload } = require("../utils/fileUpload");
+
+router.post("/", protect, upload.single("image"), createProduct);
+router.patch("/:id", protect, upload.single("image"), updateProduct);
+router.get("/", protect, getProducts);
+router.get("/:id", protect, getProduct);
+router.delete("/:id", protect, deleteProduct);
+router.post("/:id/sales", protect, addSale);
+router.post("/:id/purchases", protect, addPurchase);
+
+module.exports = router;
